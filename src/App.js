babfd7aa8e7fe50554d26013b12e034ff1366b54
joaonascimentooo <joaonascimento197@gmail.com>
2025-05-25 00:11:36 -0300

    Usando props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
 
 function App() {
 
@@ -9,6 +10,9 @@ function App() {
       <h1>Ola React</h1>
       <p>Ola {name}</p>
       <HelloWorld />
+      <SayMyName nome="João"/>
+      <SayMyName nome="Manuel"/>
+      <SayMyName nome={name}/>
     </div>
   );
 }
