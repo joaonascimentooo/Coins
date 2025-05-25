f58d9b4a4c445f7cfab5d6b090805b8bb4df5e53
joaonascimentooo <joaonascimento197@gmail.com>
2025-05-25 10:53:46 -0300

    testando css em react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
+import Frase from './components/Frase';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <h1>Ola React</h1>
       <p>Ola {name}</p>
       <HelloWorld />
+      <Frase/>
       <SayMyName nome="João"/>
       <SayMyName nome="Manuel"/>
       <SayMyName nome={name}/>
